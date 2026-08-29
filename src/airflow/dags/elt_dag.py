017bf605b227c5d59fe1edb450862f34d76d897b
Lambert Münster <lambert.muenster@gmail.com>
2026-08-29 23:50:35 +0200

    added dbt for saver data transformations

--- src/airflow/dags/elt_dag.py
+++ src/airflow/dags/elt_dag.py
@@ -1,7 +1,8 @@
 import subprocess
-from datetime import datetime 
-from airflow import DAG 
-from airflow.operators.python import PythonOperator 
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
 
 default_args = {
     'owner': 'airflow',
@@ -13,27 +14,49 @@ default_args = {
 
 def run_loading_from_smard_script():
     script_name = "/opt/airflow/loading/load_from_smard.py"
-    result = subprocess.run(["python",script_name], 
+    result = subprocess.run(["python",script_name],
             capture_output=True, text=True)
     if result.returncode != 0:
         raise Exception(f"Script failed with error: {result.stderr}")
     else:
         print(result.stdout)
-    
+
+
+def run_dbt_transformation():
+    result = subprocess.run(
+        [
+            "dbt", "run",
+            "--profiles-dir", "/opt/airflow/dbt_profiles",
+            "--project-dir", "/opt/airflow/transform",
+            "--full-refresh",
+        ],
+        capture_output=True, text=True,
+    )
+    if result.returncode != 0:
+        raise Exception(f"dbt run failed with error: {result.stderr}")
+    else:
+        print(result.stdout)
 
 dag = DAG(
-    'loading_data_from_smard',
+    'extract_load_and_transform',
     default_args=default_args,
-    description='Load the data from smard and insert to postgres.',
+    description='Load the data from smard, insert to postgres and transform with dbt.',
     start_date=datetime(2026,8,21), 
     schedule='0 * * * *',
-    catchup=False,   
+    catchup=False
+) 
+
+load_task = PythonOperator(
+    task_id='run_loading_from_smard_script',
+    python_callable=run_loading_from_smard_script,
+    dag=dag
 )
 
 
-loading_task = PythonOperator(
-    task_id='run_loading_from_smard_script',
-    python_callable=run_loading_from_smard_script,
-    dag=dag,
-) 
+transform_task = PythonOperator(
+    task_id='dbt_run',
+    python_callable=run_dbt_transformation,
+    dag=dag
+)
 
+load_task >> transform_task
